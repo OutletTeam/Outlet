224714e040a6fde6b2c84043e278ec40e0fdc7db
Uba <ubamaduka@icloud.com>
2019-06-08 16:17:22 -0400

    Initial Commit

---
+++ Outlet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Outlet
+//
+//  Created by uba on 2019-06-08.
+//  Copyright © 2019 Outlet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
